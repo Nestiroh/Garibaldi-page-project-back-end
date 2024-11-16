8c27ddd12b5f13b20a761345f982189a1c51e374
Nestor Rodriguez <nestirodriguez89@gmail.com>
2024-11-16 00:51:05 -0500

    Configuracion adicional a base de datos

--- config/db.js
+++ config/db.js
@@ -8,7 +8,11 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10, // Límite de conexiones
+    queueLimit: 0,       // Sin límite de cola
+    connectTimeout: 10000 // Tiempo de espera para conexión en ms (10 segundos)
 });
 
 // Exportar la conexión para usarla en otros archivos
